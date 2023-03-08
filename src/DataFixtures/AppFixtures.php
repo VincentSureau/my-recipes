73ea23a06dba3e395ddfb182abe6db8d9518fa22
TerenceRiss <rissterence55@gmail.com>
2023-03-08 10:54:15 +0100

    ajouté fixtures pour Season et Ingredient

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,9 @@ namespace App\DataFixtures;
 
 use Faker\Factory;
 use App\Entity\User;
+use App\Entity\Season;
+use DateTimeImmutable;
+use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -29,6 +32,58 @@ class AppFixtures extends Fixture
     
             $manager->persist($user);
         }
+
+        $ingredients=[];
+        for($i = 0; $i < 10; $i++)
+        {
+            $ingredient = new Ingredient;
+            $ingredient
+            
+                ->setType($faker->randomElement(['fruit', 'légume']))
+                ->setDescription($faker->text())
+                ->setImage($faker->imageUrl(640, 480, 'food', true))
+                ->setName($faker->word())
+            ;
+            $manager->persist($ingredient);
+            $ingredients[]=$ingredient; 
+        }
+        
+        $summerStart = new \DateTimeImmutable('2023-06-21');
+        $fallStart = new \DateTimeImmutable('2023-09-23');
+        $springStart = new \DateTimeImmutable('2023-03-20');
+        $winterStart = new \DateTimeImmutable('2023-12-21');
+
+        $summer = new Season;
+        $summer
+        ->setName('Summer')
+        ->setStartAt($summerStart)
+        ->setEndAt($fallStart)
+        ;   
+        $manager->persist($summer);
+        
+        $spring = new Season;
+        $spring
+        ->setName('Spring')
+        ->setStartAt($springStart)
+        ->setEndAt($summerStart)
+        ;
+        $manager->persist($spring);
+        
+        $winter = new Season;
+        $winter
+        ->setName('Winter')
+        ->setStartAt($winterStart)
+        ->setEndAt($springStart)
+        ;
+        $manager->persist($winter);
+        
+        $fall = new Season;
+        $fall
+        ->setName('Fall')
+        ->setStartAt($fallStart)
+        ->setEndAt($winterStart)
+        ;
+        $manager->persist($fall);
 
         $manager->flush();
     }
