9eee83de6bf1607be7e8e14315522ffe04af663f
TerenceRiss <rissterence55@gmail.com>
2023-03-09 10:46:43 +0100

    ajout css contact + image

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,11 +2,15 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Comment;
 use Faker\Factory;
 use App\Entity\User;
+use App\Entity\Recipe;
 use App\Entity\Season;
 use DateTimeImmutable;
 use App\Entity\Ingredient;
+use App\Entity\Newsletter;
+use App\Entity\RecipeIngredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -17,7 +21,7 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
         $faker = Factory::create('fr_FR');
-
+        $users=[];
         for($i = 0; $i < 10; $i++)
         {
             $user = new User;
@@ -28,26 +32,13 @@ class AppFixtures extends Fixture
                 ->setPassword($faker->sha256())
                 ->setIsSubscribed($faker->boolean())
                 ->setRoles(['ROLE_USER'])
+                
             ;
-    
+
+            $users[]=$user;
             $manager->persist($user);
         }
 
-        $ingredients=[];
-        for($i = 0; $i < 10; $i++)
-        {
-            $ingredient = new Ingredient;
-            $ingredient
-            
-                ->setType($faker->randomElement(['fruit', 'légume']))
-                ->setDescription($faker->text())
-                ->setImage($faker->imageUrl(640, 480, 'food', true))
-                ->setName($faker->word())
-            ;
-            $manager->persist($ingredient);
-            $ingredients[]=$ingredient; 
-        }
-        
         $summerStart = new \DateTimeImmutable('2023-06-21');
         $fallStart = new \DateTimeImmutable('2023-09-23');
         $springStart = new \DateTimeImmutable('2023-03-20');
@@ -84,7 +75,80 @@ class AppFixtures extends Fixture
         ->setEndAt($winterStart)
         ;
         $manager->persist($fall);
+        $seasons = [$summer, $spring, $fall,$winter];    
+
+        $ingredients=[];
+        for($i = 0; $i < 10; $i++)
+        {
+            $ingredient = new Ingredient;
+            $ingredient
+            
+                ->setType($faker->randomElement(['fruit', 'légume']))
+                ->setDescription($faker->text())
+                ->setImage($faker->imageUrl(640, 480, 'food', true))
+                ->setName($faker->word())
+                ->addSeason($faker->randomElement($seasons))
+            ;
+            $manager->persist($ingredient);
+            $ingredients[]=$ingredient; 
+        }
+        $recipes=[];
+        for($i = 0; $i < 10; $i++)
+        {
+            $recipe = new Recipe(); 
+            $recipe
+                ->setName($faker->words(4, true))
+                ->setDescription(($faker->text()))
+                ->setLevel($faker->numberBetween(1,3))
+                ->setImage($faker->imageUrl(640, 480, 'recipe', true))
+                ->addSeason($faker->randomElement($seasons))
+                ->addUser($faker->randomElement($users))
+                ;
+                $manager->persist($recipe);
+                $recipes[]=$recipe;
+        }
+        $comments=[];
+        for($i = 0; $i < 20; $i++)
+        {
+            $comment = new Comment(); 
+            $comment
+                ->setContent($faker->sentence(12))
+                ->setRecipe($faker->randomElement($recipes))
+                ->setUser($faker->randomElement($users))
+                ;
+                $manager->persist($comment);
+                $comments[]=$comment;
+        }
+
+        $newsletters=[];
+        for($i = 0; $i < 20; $i++)
+        {
+            $newsletter = new Newsletter(); 
+            $newsletter
+                ->setContent($faker->sentence(30))
+                ->setSendAt(DateTimeImmutable::createFromMutable($faker->dateTime()))
+                ->addRecipe($faker->randomElement($recipes))
+                ->addRecipe($faker->randomElement($recipes))
+                ->addRecipe($faker->randomElement($recipes))
+                ->addRecipe($faker->randomElement($recipes))
+                ;
+                $manager->persist($newsletter);
+                $newsletters[]=$newsletter;
+        }
+
+        $recipeIngredients=[];
+        for($i = 0; $i < 20; $i++)
+        {
+            $recipeIngredient = new RecipeIngredient(); 
+            $recipeIngredient
+                ->setRecipe($faker->randomElement($recipes))
+                ->setIngredient($faker->randomElement($ingredients))
+                ->setQuantity($faker->randomDigitNotNull() . $faker->randomElement(['kg', 'gramme', 'mmg']))
+                ;
+                $manager->persist($recipeIngredient);
+                $recipeIngredients[]=$recipeIngredient;
+            }
+
 
         $manager->flush();
-    }
-}
+}}
