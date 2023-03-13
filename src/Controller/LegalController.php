fa4b09eaac45085b518637c9f995969a09a48e9f
melodieot <melodie.olory@gmail.com>
2023-03-13 09:15:09 +0100

    mentions legales et footer: mise en page

--- src/Controller/LegalController.php
+++ src/Controller/LegalController.php
@@ -11,7 +11,7 @@ class LegalController extends AbstractController
     #[Route('/legal', name: 'app_legal')]
     public function index(): Response
     {
-        return $this->render('legal/index.html.twig', [
+        return $this->render('legal/mentions.html.twig', [
             'controller_name' => 'LegalController',
         ]);
     }
