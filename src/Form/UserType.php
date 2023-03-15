f4cbb9885696dbe1e130694cbe914cfcfe1ade13
Rani Derradj <raniderradj2@gmail.com>
2023-03-15 11:14:50 +0100

    modif du new et edit

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,19 +3,31 @@
 namespace App\Form;
 
 use App\Entity\User;
+// use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            // ->add('roles')  
-            ->add('password')
-            ->add('username')
+            ->add('email',EmailType::class,[
+
+                'attr'=> array('class' =>'form-label'),
+            ])
+            //->add('roles')
+            ->add('password', PasswordType::class, [
+                'attr' => ['class' => 'form-label'],
+            ])
+            ->add('username',TextType::class,[
+                'attr'=> array('class' =>'form-label'),
+            ])
             ->add('avatar')
             ->add('isSubscribed')
             ->add('likes')
