117b278175c3e17c74878d059703c49a55fc10c2
melodieot <melodie.olory@gmail.com>
2023-03-16 15:27:47 +0100

    integration vich pour recipe

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -6,17 +6,22 @@ use App\Entity\Recipe;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class RecipeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la recette'
+            ])
+            ->add('imageFile', VichImageType::class)
             ->add('description')
             ->add('level')
-            ->add('image')
-            ->add('liked_by')
             ->add('seasons')
         ;
     }
