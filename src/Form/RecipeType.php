bb48f39c0b23b54b57a6792fd65e271db6d20f74
Vincent Sureau <vincentsureau5@gmail.com>
2023-03-17 14:15:16 +0100

    modifié form pour utilisation de composant twig

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -8,7 +8,8 @@ use Symfony\Component\Form\AbstractType;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\UX\LiveComponent\Form\Type\LiveCollectionType;
 
 class RecipeType extends AbstractType
 {
@@ -19,16 +20,25 @@ class RecipeType extends AbstractType
             ->add('imageFile', VichFileType::class, [
                 'required' => false,
                 'allow_delete' => true,
-                'asset_helper' => true,])
-                ->add('images', CollectionType::class, [
-                    'entry_type' => MediaType::class,
-                    'entry_options' => ['label' => false],
-                    'allow_add' => true,
-                    'by_reference' => false,
-                ])
-            ->add('description')
-            ->add('level')
-            ->add('seasons')
+                'asset_helper' => true,
+                'label' => 'Image de mise en avant'
+            ])
+            ->add('images', LiveCollectionType::class, [
+                'entry_type' => MediaType::class,
+                'entry_options' => ['label' => false],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description'
+            ])
+            ->add('level', null, [
+                'label' => 'Difficulté'
+            ])
+            ->add('seasons', null, [
+                'label' => 'Saisons'
+            ])
         ;
     }
 
