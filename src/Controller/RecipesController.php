722b364b92d41eedf21d0ebce20207b619c4a4f0
clarkSTRP <clarkSTRP~>
2023-03-20 12:33:19 +0100

    pagination ok

--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -3,18 +3,32 @@
 namespace App\Controller;
 
 use App\Repository\RecipeRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RecipesController extends AbstractController
 {
     #[Route('/recipes', name: 'recipes')]
-    public function index(RecipeRepository $recipeRepository): Response
+    public function index(RecipeRepository $recipeRepository ,PaginatorInterface $paginator, Request $request): Response
     {
+
         $recipes = $recipeRepository->findAll();
+        $pagination = $paginator->paginate(
+            $recipes, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            2 /*limit per page*/
+        );
         return $this->render('recipes/index.html.twig', [
             'recipes' => $recipes,
+            'pagination' => $pagination,
         ]);
+
     }
+    // App\Controller\ArticleController.php
+    
+    
 }
