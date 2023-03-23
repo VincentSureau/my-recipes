d9b966fbc08e61a9145ecb9d9aae61f4e82dc231
Nesami Haiiro <20476281+citrusMarmelade@users.noreply.github.com>
2023-03-23 12:13:39 +0100

    Toggle du highlight

--- public/js/recipe-selector.js
+++ public/js/recipe-selector.js
@@ -1,15 +1,20 @@
 const list = document.querySelector(".recipe-list");
 
+list.querySelectorAll(".card")
+
 list.addEventListener("click", (event) => {
     const elem = event.target.closest(".card");
     if (!elem) return;
     if (!list.contains(elem)) return;
 
-    console.log(elem);
-    highlight(elem);
-})
+    const recipeId = elem.dataset.recipeId;
 
-function highlight(element) {
-    element.style.setProperty("box-shadow","0 .5rem 1rem red", "important");
-}
+    if (elem.classList.toggle("selected")) {
+        // selection
+        console.log("selection", recipeId);
+    } else {
+        // deselection
+        console.log("deselection", recipeId);
+    }
+})
 
